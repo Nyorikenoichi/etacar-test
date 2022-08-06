d59d789e6fb46c700b9274e70052183a34fed34c
nyorikenoichi <malsef.maks@yandex.ru>
2022-08-06 18:54:04 +0300

    create crypto table and pagination for it

---
+++ src/core/helpers/formatFloat.ts
@@ -0,0 +1,13 @@
+export const formateFloat = (value: string): string => {
+  const numericalValue = parseFloat(value);
+  if (numericalValue > 1000000000) {
+    return `${(numericalValue / 1000000000).toFixed(2)}b`;
+  }
+  if (numericalValue > 1000000) {
+    return `${(numericalValue / 1000000).toFixed(2)}m`;
+  }
+  if (numericalValue > 1000) {
+    return `${(numericalValue / 1000).toFixed(2)}k`;
+  }
+  return numericalValue.toFixed(2).toString();
+};
