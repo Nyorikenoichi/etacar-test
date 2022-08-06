54f6df74a68e8fb105418233584444bae258c211
nyorikenoichi <malsef.maks@yandex.ru>
2022-08-06 04:34:39 +0300

    create cryptoTable and pagimation system for it

--- src/core/components/cryptoTable/cryptoTable.tsx
+++ src/core/components/cryptoTable/cryptoTable.tsx
@@ -1,5 +1,61 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { CryptoTablePagination } from '../cryptoTablePagination/cryptoTablePagination';
+import data from './mock-data.json';
 
-export const CryptoTable = (): JSX.Element => {
-  return <div>crypto list</div>;
+const PageSize = 10;
+
+export const CryptoTable = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  return (
+    <div className="stack stack_vertical crypto-table__container">
+      <table style={{ width: '100%' }}>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Market Cap</th>
+            <th>VWAP(24Hr)</th>
+            <th>Supply</th>
+            <th>Volume(24Hr)</th>
+            <th>Change(24Hr)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.rank}</td>
+                <td>{item.name}</td>
+                <td>{parseFloat(item.priceUsd).toFixed(2)}</td>
+                <td>{parseFloat(item.marketCapUsd).toFixed(2)}</td>
+                <td>{parseFloat(item.vwap24Hr).toFixed(2)}</td>
+                <td>{parseFloat(item.supply).toFixed(2)}</td>
+                <td>{parseFloat(item.volumeUsd24Hr).toFixed(2)}</td>
+                <td>
+                  {typeof item.changePercent24Hr === 'string'
+                    ? parseFloat(item.changePercent24Hr).toFixed(2)
+                    : ''}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <CryptoTablePagination
+        currentPage={currentPage}
+        totalCount={data.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+        siblingCount={1}
+      />
+    </div>
+  );
 };
---
+++ src/core/components/cryptoTablePagination/cryptoTablePagination.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { dots, usePagination } from '../../hooks/usePagination';
+
+interface CryptoTablePaginationProps {
+  onPageChange: (x: number) => void;
+  totalCount: number;
+  siblingCount: number;
+  currentPage: number;
+  pageSize: number;
+}
+
+export const CryptoTablePagination = ({
+  onPageChange,
+  totalCount,
+  siblingCount,
+  currentPage,
+  pageSize,
+}: CryptoTablePaginationProps): JSX.Element | null => {
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  const lastPage = paginationRange[paginationRange.length - 1];
+  return (
+    <ul className="pagination__container">
+      <li
+        className={`pagination__item${
+          currentPage === 1 ? ' pagination__item_disabled' : ''
+        }`}
+        onClick={onPrevious}
+      >
+        <div className="pagination__arrow pagination__arrow_left" />
+      </li>
+      {paginationRange.map((pageNumber) => {
+        if (pageNumber === dots) {
+          return (
+            <li className="pagination__item pagination__item_dots">&#8230;</li>
+          );
+        }
+
+        return (
+          <li
+            key={pageNumber}
+            className={`pagination__item${
+              currentPage === pageNumber ? ' pagination__item_selected' : ''
+            }`}
+            onClick={() => onPageChange(+pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        );
+      })}
+      <li
+        className={`pagination__item${
+          currentPage === lastPage ? ' pagination__item_disabled' : ''
+        }`}
+        onClick={onNext}
+      >
+        <div className="pagination__arrow pagination__arrow_right" />
+      </li>
+    </ul>
+  );
+};
