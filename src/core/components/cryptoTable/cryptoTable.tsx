30d6955656bbc562b71e3d2d2e6e94c461c6d0e8
nyorikenoichi <malsef.maks@yandex.ru>
2022-08-08 03:11:10 +0300

    create preloader, use it when CryptoTable loads

--- src/core/components/cryptoTable/cryptoTable.tsx
+++ src/core/components/cryptoTable/cryptoTable.tsx
@@ -12,7 +12,9 @@ import { fetchCurrencies } from '../../redux/slices/currenciesSlice';
 const PageSize = 14;
 
 export const CryptoTable = () => {
-  const currencies = useAppSelector((state) => state.currency.currencies);
+  const { currencies, error, loading } = useAppSelector(
+    (state) => state.currency
+  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -47,81 +49,87 @@ export const CryptoTable = () => {
   }, [currentPage, currencies]);
 
   return (
-    <div className="stack stack_vertical crypto-table__container">
-      <table className="crypto-table">
-        <thead>
-          <tr className="crypto-table__row crypto-table__row_header">
-            <th className="crypto-table__cell">Rank</th>
-            <th className="crypto-table__cell crypto-table__cell_align-left crypto-table__cell_wide">
-              Name
-            </th>
-            <th className="crypto-table__cell">Price</th>
-            <th className="crypto-table__cell">Market Cap</th>
-            <th className="crypto-table__cell">VWAP(24Hr)</th>
-            <th className="crypto-table__cell">Supply</th>
-            <th className="crypto-table__cell">Volume(24Hr)</th>
-            <th className="crypto-table__cell">Change(24Hr)</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {currentTableData.map((item) => {
-            return (
-              <tr
-                className="crypto-table__row crypto-table__row_body crypto-table__row_clickable"
-                key={item.id}
-                onClick={onGoToInfo(item.id)}
-              >
-                <td className="crypto-table__cell">{item.rank}</td>
-                <td className="crypto-table__cell crypto-table__cell_align-left">
-                  {item.name}
-                </td>
-                <td className="crypto-table__cell">
-                  ${formatFloat(item.priceUsd)}
-                </td>
-                <td className="crypto-table__cell">
-                  ${formatFloat(item.marketCapUsd)}
-                </td>
-                <td className="crypto-table__cell">
-                  ${formatFloat(item.vwap24Hr)}
-                </td>
-                <td className="crypto-table__cell">
-                  {formatFloat(item.supply)}
-                </td>
-                <td className="crypto-table__cell">
-                  ${formatFloat(item.volumeUsd24Hr)}
-                </td>
-                <td className="crypto-table__cell">
-                  {typeof item.changePercent24Hr === 'string'
-                    ? `${formatFloat(item.changePercent24Hr)}%`
-                    : ''}
-                </td>
-                <td className="crypto-table__cell">
-                  <div
-                    className="cryptoTable__cell_button"
-                    onClick={onAddCurrency(item)}
-                  >
-                    +
-                  </div>
-                </td>
+    <>
+      {loading && <div className="preloader" />}
+      {!loading && error ? <div>Error: {error}</div> : null}
+      {!loading && currencies.length ? (
+        <div className="stack stack_vertical crypto-table__container">
+          <table className="crypto-table">
+            <thead>
+              <tr className="crypto-table__row crypto-table__row_header">
+                <th className="crypto-table__cell">Rank</th>
+                <th className="crypto-table__cell crypto-table__cell_align-left crypto-table__cell_wide">
+                  Name
+                </th>
+                <th className="crypto-table__cell">Price</th>
+                <th className="crypto-table__cell">Market Cap</th>
+                <th className="crypto-table__cell">VWAP(24Hr)</th>
+                <th className="crypto-table__cell">Supply</th>
+                <th className="crypto-table__cell">Volume(24Hr)</th>
+                <th className="crypto-table__cell">Change(24Hr)</th>
+                <th />
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Pagination
-        currentPage={currentPage}
-        totalCount={currencies.length}
-        pageSize={PageSize}
-        onPageChange={(page) => setCurrentPage(page)}
-        siblingCount={1}
-      />
+            </thead>
+            <tbody>
+              {currentTableData.map((item) => {
+                return (
+                  <tr
+                    className="crypto-table__row crypto-table__row_body crypto-table__row_clickable"
+                    key={item.id}
+                    onClick={onGoToInfo(item.id)}
+                  >
+                    <td className="crypto-table__cell">{item.rank}</td>
+                    <td className="crypto-table__cell crypto-table__cell_align-left">
+                      {item.name}
+                    </td>
+                    <td className="crypto-table__cell">
+                      ${formatFloat(item.priceUsd)}
+                    </td>
+                    <td className="crypto-table__cell">
+                      ${formatFloat(item.marketCapUsd)}
+                    </td>
+                    <td className="crypto-table__cell">
+                      ${formatFloat(item.vwap24Hr)}
+                    </td>
+                    <td className="crypto-table__cell">
+                      {formatFloat(item.supply)}
+                    </td>
+                    <td className="crypto-table__cell">
+                      ${formatFloat(item.volumeUsd24Hr)}
+                    </td>
+                    <td className="crypto-table__cell">
+                      {typeof item.changePercent24Hr === 'string'
+                        ? `${formatFloat(item.changePercent24Hr)}%`
+                        : ''}
+                    </td>
+                    <td className="crypto-table__cell">
+                      <div
+                        className="cryptoTable__cell_button"
+                        onClick={onAddCurrency(item)}
+                      >
+                        +
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <Pagination
+            currentPage={currentPage}
+            totalCount={currencies.length}
+            pageSize={PageSize}
+            onPageChange={(page) => setCurrentPage(page)}
+            siblingCount={1}
+          />
+        </div>
+      ) : null}
       {isModalOpen && (
         <ModalAddCurrency
           setIsOpen={setIsModalOpen}
           currency={selectedCurrency}
         />
       )}
-    </div>
+    </>
   );
 };
