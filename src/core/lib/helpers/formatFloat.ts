3ba809f92f1602aaa86606bfa46d6e9e03420386
nyorikenoichi <malsef.maks@yandex.ru>
2022-08-10 23:13:43 +0300

    change file structure

--- src/core/lib/helpers/formatFloat.ts
+++ src/core/lib/helpers/formatFloat.ts
@@ -12,5 +12,9 @@ export const formatFloat = (value: string | number): string => {
   if (numericalValue > 1000) {
     return `${(numericalValue / 1000).toFixed(2)}k`;
   }
-  return numericalValue.toFixed(2).toString();
+  if (numericalValue < 0.01 && numericalValue != 0) {
+    const firstNonZeroDigit = Math.ceil(Math.abs(Math.log10(numericalValue)));
+    return numericalValue.toFixed(firstNonZeroDigit);
+  }
+  return numericalValue.toFixed(2);
 };
